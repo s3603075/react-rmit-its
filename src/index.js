4f1880fa65c9e0acfb86027e3bb8e90a71352986
rmit-s3603075-danny-ho <s3603075@student.rmit.edu.au>
2017-09-24 08:27:18 +1000

    Auth and other stuff

--- src/index.js
+++ src/index.js
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import firebase from 'firebase';
+import {BrowserRouter} from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const config = {
+    apiKey: "AIzaSyDaT-A208XALPjEegzPB7WoZl5NPR5dPiE",
+    authDomain: "rmit-its.firebaseapp.com",
+    databaseURL: "https://rmit-its.firebaseio.com",
+    projectId: "rmit-its",
+    storageBucket: "rmit-its.appspot.com",
+    messagingSenderId: "883266328240"
+};
+firebase.initializeApp(config);
+
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+, document.getElementById('root'));
 registerServiceWorker();
+

0520c7016edf62780e40f61e1ec6589e2b5ca244
rmit-s3603075-danny-ho <s3603075@student.rmit.edu.au>
2017-10-08 16:38:18 +1100

    set api stuff

--- src/components/dashboard/Details.js
+++ src/components/dashboard/Details.js
@@ -6,11 +6,32 @@ import { Route, Redirect } from 'react-router';
 
 class Details extends Component {
     state = {
-        showModal: true
+        showModal: true,
+        status: null
     }
 
     closeModal = (bool) => {
         this.setState({showModal: bool});
+    }
+
+    handleStatusChange = (e) => {
+        this.setState({
+            status: e.target.value
+        });      
+    }
+
+    changeStatus = () =>  {
+        if(this.state.status === null) {
+            return;
+        }
+        fetch(apiurl+"api/tickets/status/"+this.props.ticket.id+"?status="+this.state.status, {
+            method: 'PUT'
+        })
+            .then((response) => response.json())
+            .then((responseJson) => {
+                console.log(responseJson);
+            })
+        alert('Status changed!'); 
     }
 
     render () {
@@ -23,6 +44,15 @@ class Details extends Component {
                         <Modal.Body>
                             <h2>Status</h2>
                                 {this.props.ticket.status}
+                                <select className="form-control" onChange={this.handleStatusChange} defaultValue="-1">
+                                    <option value="-1" defaultValue disabled>Select priority</option>
+                                        <option value={1}>Unresolved</option>
+                                        <option value={2}>Resolved</option>
+                                        <option value={3}>In Progress</option>
+                                </select>
+                                <div className="clearfix"><br/>
+                                        <Button className="pull-right" bsStyle="success" onClick={this.changeStatus}>Assign</Button>
+                                </div>
                             <h2>Issue</h2>
                             <div>
                                 {this.props.ticket.issue}
